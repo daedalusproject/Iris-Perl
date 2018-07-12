ea5fcf82cf7ff5211df023a982a3abf04fddec48
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2018-07-12 18:15:30 +0200

    Mocked a non working funcion LOLgit add .git add ., Fix it....

--- lib/Daedalus/Iris/Email.pm
+++ lib/Daedalus/Iris/Email.pm
@@ -8,7 +8,7 @@ use Moose;
 use Carp qw /croak/;
 use Data::Validate::Domain qw(is_domain);
 use Email::Valid;
-use Email::Sender::Simple;
+use Email::Sender::Simple qw(sendmail);
 use Email::Stuffer;
 use Email::Sender::Transport::SMTPS ();
 use base qw( Daedalus::Iris );
@@ -97,7 +97,7 @@ sub _send {
 
         }
     );
-    Email::Sender::Simple::sendmail( $email, { transport => $transport } );
+    sendmail( $email, { transport => $transport } );
 
 }
 
--- t/lib/TestsFor/Daedalus/Iris/Email.pm
+++ t/lib/TestsFor/Daedalus/Iris/Email.pm
@@ -16,45 +16,42 @@ sub startup : Tests(startup) {
 }
 
 sub send_email : Tests(1) {
-my $IRIS = Daedalus::Iris->new('email');
+    my $IRIS = Daedalus::Iris->new('email');
 
-my $random_string = new String::Random;
-my $random        = $random_string->randpattern( 's' x 32 );
+    my $random_string = new String::Random;
+    my $random        = $random_string->randpattern( 's' x 32 );
 
-my $mocked_email = mock();
+    my $mocked_email = mock();
 
-our $smtpserver   = $ENV{'IRIS_SMTP_SERVER'};
-our $smtpport     = $ENV{'IRIS_SMTP_PORT'};
-our $smtpuser     = $ENV{'IRIS_SMTP_USER'};
-our $smtppassword = $ENV{'IRIS_SMTP_PASSWORD'};
-our $emailto      = $ENV{'IRIS_EMAIL_TO'};
-our $emailfrom    = $ENV{'IRIS_EMAIL_FROM'};
+    our $smtpserver   = $ENV{'IRIS_SMTP_SERVER'};
+    our $smtpport     = $ENV{'IRIS_SMTP_PORT'};
+    our $smtpuser     = $ENV{'IRIS_SMTP_USER'};
+    our $smtppassword = $ENV{'IRIS_SMTP_PASSWORD'};
+    our $emailto      = $ENV{'IRIS_EMAIL_TO'};
+    our $emailfrom    = $ENV{'IRIS_EMAIL_FROM'};
 
-my $iris = $IRIS->new(
-    {
-        id          => $random,
-        smtpserver  => $smtpserver,
-        smtpport    => $smtpport,
-        smtpuser    => $smtpuser,
-        smtpassword => $smtppassword,
-        emailfrom   => $emailfrom,
-        emailto     => $emailto,
-        subject     => 'some imporant subject',
-        body        => 'Hello',
-    }
-);
+    my $iris = $IRIS->new(
+        {
+            id          => $random,
+            smtpserver  => $smtpserver,
+            smtpport    => $smtpport,
+            smtpuser    => $smtpuser,
+            smtpassword => $smtppassword,
+            emailfrom   => $emailfrom,
+            emailto     => $emailto,
+            subject     => 'some imporant subject',
+            body        => 'Hello',
+        }
+    );
 
-ok $iris->send(), "Send E-mail";
+    ok $iris->send(), "Send E-mail";
 
 }
 
 sub mock {
 
     my $mocked_email = Test::MockModule->new('Email::Sender::Simple');
-    $mocked_email->mock(
-    'sendmail',
-    sub {return 1;}
-    );
+    $mocked_email->mock( 'send', sub { return 1; } );
 
     return $mocked_email;
 
